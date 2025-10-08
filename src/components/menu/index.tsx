8e570587b5629b9ccca63f893875cedb2e7bf545
grzegorz.gozdyra <grzegorz.gozdyra@orba.co>
2025-10-08 16:43:43 +0200

    create menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,32 +1,49 @@
-import { Home, Phone } from 'lucide-react'
+import logoSvg from '@/assets/logo.svg'
 
 const Menu = () => {
   const menuItems = [
     {
-      icon: Home,
       label: 'Dashboard',
       path: '/',
     },
     {
-      icon: Phone,
-      label: 'Latest calls',
+      label: 'Calls',
       path: '/latest-calls',
+    },
+    {
+      label: 'Text',
+      path: '/text',
     },
   ]
 
   return (
-    <ul className="py-4 space-y-2 bg-sidebar-bg h-full">
-      {menuItems.map((item) => {
-        return (
-            <li className="mb-10">
-              <a href={item.path} className="no-underline flex flex-col items-center">
-                  <span className="inline-block w-[70px] h-[66px] bg-white mb-2 rounded-[10px]"></span>
-                  <span className="text-xs text-white">{item.label}</span>
-                </a>
-          </li>
-        )
-      })}
-    </ul>
+    <div className="flex flex-col items-center justify-between py-6 bg-sidebar-bg h-full text-white text-sm font-bold leading-[18px]">
+      <div>
+        <div className="flex justify-center">
+          <img 
+            src={logoSvg} 
+            alt="Logo" 
+            className="w-[66px] h-[55px] mb-12"
+          />
+        </div>
+        <ul className="space-y-2">
+          {menuItems.map((item) => {
+            return (
+                <li className="mb-10">
+                  <a href={item.path} className="no-underline flex flex-col items-center">
+                      <span className="inline-block w-[70px] h-[66px] bg-white mb-2 rounded-[10px]"></span>
+                      <span className="">{item.label}</span>
+                    </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <div>
+        <div className="w-[69px] h-[69px] bg-custom-gray-1 rounded-full"></div>
+        <p className="text-center">Agent</p>
+      </div>
+    </div>
   )
 }
 
